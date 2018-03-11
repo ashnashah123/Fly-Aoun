82f552acedd1b5945339db30f7b80c351ffd2527
KennethSee <kennethseede@gmail.com>
2018-03-11 03:46:35 +0000

    you done fucked up now

--- src/app/Search-Services/search.service.ts
+++ src/app/Search-Services/search.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Carrier } from './../Server-Objects/carrier'
-import { Place } from './../Server-Objects/place'
-import { Segment } from './../Server-Objects/segment' 
+import { Carrier } from './../Server-Objects/carrier';
+import { Place } from './../Server-Objects/place';
+import { Segment } from './../Server-Objects/segment';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,10 +11,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class SearchService {
     constructor() {}
-    
 
-    getSegment(): Observable<Segment[]> {
-        return of();
-      }
+
+    getResults(startingCity: String, endingCity: String, startingDate: Date, 
+        endingDate: Date, budgetRestriction: Number): Observable<Segment[]> {
+        return;
+    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material';
 import {MatSliderModule} from '@angular/material/slider';
 import {worldMap} from './MapComponent/map.component';
-import {searchBar} from './SearchComponent/search.component';import {MatButtonModule} from '@angular/material/button';
-
+// import {searchBar} from './SearchComponent/search.component';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {SearchBarComponent} from './SearchComponent/search.component';
+import {MatSelectModule} from '@angular/material/select';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { SearchService } from './Search-Services/search.service';
 
 
@@ -17,17 +21,21 @@ import { SearchService } from './Search-Services/search.service';
   declarations: [
     AppComponent,
     worldMap,
-    searchBar
+    // searchBar,
+    SearchBarComponent,
+
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatSliderModule
+    MatSliderModule,
+    MatSelectModule
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
